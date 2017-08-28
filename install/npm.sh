84b3cceb3f5bc82d0f8f2abd383c0d545eeccf4e
Jürgen Messner <juergen.messner@cornelsen.de>
2017-08-28 16:33:17 +0200

    removed broken esctags and jsctags from npm install added yarn to npm deps

--- install/npm.sh
+++ install/npm.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
-npm install -g jsctags
-npm install -g es-ctags
+# npm install -g jsctags
+# npm install -g es-ctags
 npm install -g eslint
 npm install -g babel-eslint
 npm install -g eslint-plugin-react
+npm install -g yarn
