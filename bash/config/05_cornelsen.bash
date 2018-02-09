16904c74b4bd2ced4c13b90222a46b196f440c42
Jürgen Messner <juergen.messner@cornelsen.de>
2018-02-09 11:46:27 +0100

    removed cornelsen specific configs

--- bash/config/05_cornelsen.bash
+++
@@ -1,8 +0,0 @@
-
-beginswith() { case $2 in "$1"*) true;; *) false;; esac; }
-
-CURRENT_NETWORK_LOCATION="$(scselect | awk '{if ($1=="*") print $3}')"
-if beginswith "(Cornelsen" $CURRENT_NETWORK_LOCATION; then
-    echo "initializing Cornelsen specific environment"
-    source $DOTFILES/environment/cornelsen
-fi
