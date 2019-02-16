76ff15210babc963ca2277509722874ce1ca4e11
Jürgen Messner <juergen.messner@cornelsen.de>
2019-02-16 21:19:42 +0100

    add zsh docker plugin, removed unnecessary zsh async

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -10,9 +10,9 @@ source $ZPLUG_HOME/init.zsh
 # Plugins to install
 
 zplug "plugins/vi-mode", from:oh-my-zsh
+zplug "plugins/docker", use:_docker, from:oh-my-zsh
 zplug "plugins/docker-compose", from:oh-my-zsh
 zplug "denysdovhan/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
-zplug "mafredri/zsh-async", on:"sindresorhus/pure"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-autosuggestions"
 
