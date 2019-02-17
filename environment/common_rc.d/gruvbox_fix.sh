91160cce9c5f1e2b589e626883108191a34a870f
jirgn <jirgn76@googlemail.com>
2019-02-17 01:01:45 +0100

    fixed loading of gruvbox fix

--- environment/common_rc.d/gruvbox_fix.sh
+++ environment/common_rc.d/gruvbox_fix.sh
@@ -1,3 +1,5 @@
+gruvboxFix=$DOTFILES/config/nvim/plugged/gruvbox/gruvbox_256palette.sh
+
 if [[ -f $gruvboxFix ]]; then
-    source "$HOME/.config/nvim/plugged/gruvbox/gruvbox_256palette.sh"
+    source $gruvboxFix
 fi
