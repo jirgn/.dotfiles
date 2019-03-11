e7190be50c87ea75a12e74b53297c5de4dc2a276
jirgn <jirgn76@googlemail.com>
2019-03-11 14:54:33 +0100

    simplyfied prompt

--- zsh/config/spaceship-prompt.zsh
+++ zsh/config/spaceship-prompt.zsh
@@ -1,20 +1,16 @@
-SPACESHIP_CHAR_SYMBOL='  '
-SPACESHIP_HG_SHOW=false
-SPACESHIP_HG_BRANCH_SHOW=false
-SPACESHIP_HG_STATUS_SHOW=false
-SPACESHIP_RUBY_SHOW=false
-SPACESHIP_ELM_SHOW=false
-SPACESHIP_XCODE_SHOW_LOCAL=false
-SPACESHIP_XCODE_SHOW_GLOBAL=false
-SPACESHIP_SWIFT_SHOW_LOCAL=false
-SPACESHIP_GOLANG_SHOW=false
-SPACESHIP_RUST_SHOW=false
-SPACESHIP_HASKELL_SHOW=false
-SPACESHIP_JULIA_SHOW=false
-SPACESHIP_AWS_SHOW=false
-SPACESHIP_CONDA_SHOW=false
-SPACESHIP_DOTNET_SHOW=false
-SPACESHIP_EMBER_SHOW=false
-SPACESHIP_KUBECONTEXT_SHOW=false
-SPACESHIP_TERRAFORM_SHOW=false
-SPACESHIP_EXEC_TIME_SHOW=false
+SPACESHIP_PROMPT_ORDER=(
+  user          # Username section
+  dir           # Current directory section
+  host          # Hostname section
+  git           # Git section (git_branch + git_status)
+  hg            # Mercurial section (hg_branch  + hg_status)
+  exec_time     # Execution time
+  line_sep      # Line break
+  vi_mode       # Vi-mode indicator
+  jobs          # Background jobs indicator
+  exit_code     # Exit code section
+  char          # Prompt character
+)
+SPACESHIP_PROMPT_ADD_NEWLINE=false
+SPACESHIP_CHAR_SYMBOL="❯"
+SPACESHIP_CHAR_SUFFIX=" "
