6da915c438b5476081fd25911f43cd04d049d617
jirgn <jirgn76@googlemail.com>
2019-03-26 16:46:57 +0100

    fixed sidebar open -- minor improvements

--- environment/common_rc.d/neomutt.sh
+++ environment/common_rc.d/neomutt.sh
@@ -1,7 +1,10 @@
+# ^y by default bound by shell
+# disable dsusp so we can bind ^y for vim like scolling in mutt
 neomutt()
 {
   old=$(stty -g)                               # Capture old termio params
   stty dsusp undef                             # Disable DSUSP
+  stty discard undef                             # Disable DSUSP
   trap "rc=$?; stty $old; exit $rc" 0 1 2 3 15 # Restore termios on interrupt
   /usr/local/bin/neomutt "$@"                     # Run mutt
   stty $old                                    # Restore termios on exit
