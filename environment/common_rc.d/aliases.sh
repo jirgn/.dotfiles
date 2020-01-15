572d9e5bededd70a3f5f30398a8e102182eb0817
Jürgen Messner <juergen.messner@cornelsen.de>
2020-01-15 10:18:25 +0100

    add ruby to nvim startup paths

--- environment/common_rc.d/aliases.sh
+++ environment/common_rc.d/aliases.sh
@@ -5,7 +5,7 @@ alias ll='ls -Gl'
 alias la='ls -lAhG'
 alias l='ls -CFG'
 # ensure lazy loded nvm and python are present as well as the project specific binaries (eslint, phpunit ...)
-alias nvim='node --version && python --version && PATH=./node_modules/.bin:$PATH nvim'
+alias nvim='node --version && python --version && ruby --version && PATH=./node_modules/.bin:$PATH nvim'
 alias vi='nvim'
 # alias vim='nvim'
 alias gemacs='node --version && python --version && /Applications/Emacs.app/Contents/MacOS/Emacs >/dev/null &'
