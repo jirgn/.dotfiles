2978b968a133254da8f8738a392f0ce277add44b
Jürgen Messner <juergen.messner@cornelsen.de>
2020-02-15 21:57:34 +0100

    fix fkill for fzf kill command

--- environment/common_profile.d/fzf.sh
+++ environment/common_profile.d/fzf.sh
@@ -9,18 +9,3 @@ fi
 if [[ ! "$MANPATH" == */usr/local/opt/fzf/man* && -d "/usr/local/opt/fzf/man" ]]; then
   export MANPATH="$MANPATH:/usr/local/opt/fzf/man"
 fi
-
-# Addidional Funcions
-# --------
-fkill() {
-  local pid
-  pid=`ps -ef | sed 1d | fzf -m | awk '{print $2}'`
-
-  if [ "x$pid" != "x" ]
-  then
-    echo $pid | xargs kill -${1:-9}
-  fi
-}
-
-alias fkill=fkill
-
---
+++ environment/common_rc.d/fzf.sh
@@ -0,0 +1,14 @@
+# Addidional Funcions
+# --------
+fkill() {
+  local pid
+  pid=`ps -ef | sed 1d | fzf -m | awk '{print $2}'`
+
+  if [ "x$pid" != "x" ]
+  then
+    echo $pid | xargs kill -${1:-9}
+  fi
+}
+
+alias fkill=fkill
+
