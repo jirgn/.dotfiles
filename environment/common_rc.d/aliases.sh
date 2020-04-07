62caa2ae1064e439d11ea2153a2601116efb3d8e
Jürgen Messner <juergen.messner@cornelsen.de>
2020-04-07 19:32:34 +0200

    remove ruby loading in nvim alias

--- environment/common_rc.d/aliases.sh
+++ environment/common_rc.d/aliases.sh
@@ -5,7 +5,7 @@ alias ll='ls -Gl'
 alias la='ls -lAhG'
 alias l='ls -CFG'
 # ensure lazy loded nvm and python are present as well as the project specific binaries (eslint, phpunit ...)
-alias nvim='node --version && python --version && ruby --version && PATH=./node_modules/.bin:$PATH nvim'
+alias nvim='node --version && python --version && PATH=./node_modules/.bin:$PATH nvim'
 alias vi='nvim'
 # alias vim='nvim'
 alias gemacs='node --version && python --version && /Applications/Emacs.app/Contents/MacOS/Emacs >/dev/null &'
@@ -17,3 +17,5 @@ alias top='top -o cpu'
 alias grep="grep --exclude='all-wcprops' --exclude='*.tmp' --exclude='entries' --exclude='*.svn-base' --exclude='*.svn*' "
 alias tmux="TERM=screen-256color-bce tmux"
 # alias php="${BREW_PREFIX}${PHPROOT}/bin/php"
+
+alias tstamp='date "+%Y%m%d%H%m%S"'
