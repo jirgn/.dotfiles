f5299a3087145235601f62efb3f188dd8ca2c300
Jirgn <jirgn76@googlemail.com>
2021-06-30 22:02:33 +0200

    pyenv - fix for updated version (pacman)

--- environment/common_profile.sh
+++ environment/common_profile.sh
@@ -12,14 +12,17 @@ export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --no-ignore-vcs --e
 export FZF_DEFAULT_OPTS="--ansi"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export COMPOSER_MEMORY_LIMIT=-1
+export GEM_HOME="${HOME}/.gem"
+export PYENV_ROOT="$HOME/.pyenv"
+
 # ------------------------------------------
 # path definition
 # ------------------------------------------
 PATH=$PATH:/usr/local/bin:/usr/local/sbin # User binaries
-PATH=$PATH:$HOME/bin                      # Personal binaries
 PATH=$PATH:$DOTFILES/bin                  # Personal binaries
 PATH=$PATH:$HOME/.local/bin               # pipsi libs
 PATH=$PATH:$PYENV_ROOT
+PATH=$PATH:$GEM_HOME/ruby/2.7.0/bin
 
 localprofile=$DOTFILES/environment/local_profile.sh
 if [[ -f $localprofile ]]; then
