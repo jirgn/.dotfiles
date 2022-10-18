e75e8fde4bc9d0349a30422cf7c0f9ecbb0ead28
Jirgn <jirgn76@googlemail.com>
2022-10-18 07:29:32 +0200

    add functions to streamline logseq <-> org-roam

---
+++ doom.d/custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-safe-remote-resources
+   '("\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-NAME\\.setup\\'")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-block ((t (:background nil))))
+ '(org-document-info ((t (:inherit 'nano-face-faded))))
+ '(org-document-title ((t (:height 1.6 :weight bold))))
+ '(org-level-1 ((t (:height 1.3 :weight extrabold :slant normal))))
+ '(org-level-2 ((t (:height 1.2 :weight bold :slant normal))))
+ '(org-level-3 ((t (:height 1.1 :weight regular :slant normal)))))
