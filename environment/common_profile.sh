b1f3c01bda39fe2a10ae7672a4d462b6bae064f5
Jirgn <jirgn76@googlemail.com>
2023-01-23 09:03:28 +0100

    chore: activate asdf and remove pyenv, ..

--- environment/common_profile.sh
+++ environment/common_profile.sh
@@ -7,13 +7,11 @@ export EDITOR=nvim
 export MAKEOPTS="-j9"
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
-export PYENV_ROOT="`pyenv root`"
 export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --no-ignore-vcs --exclude .git'
 export FZF_DEFAULT_OPTS="--ansi"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export COMPOSER_MEMORY_LIMIT=-1
 export GEM_HOME="${HOME}/.gem"
-export PYENV_ROOT="$HOME/.pyenv"
 export XDG_DATA_HOME="${HOME}/.local/share"
 export XDG_CONFIG_HOME="${HOME}/.config"
 export XDG_CACHE_HOME="${HOME}/.cache"
@@ -27,8 +25,7 @@ PATH=$PATH:/usr/local/bin:/usr/local/sbin # User binaries
 PATH=$PATH:$DOTFILES/bin                  # Personal binaries
 PATH=$PATH:$HOME/.local/bin               # pipsi libs
 PATH=$PATH:$HOME/.cargo/bin               # cargo libs
-PATH=$PATH:$PYENV_ROOT
-PATH=$PATH:$GEM_HOME/ruby/2.7.0/bin
+PATH=$PATH:$HOME/.yarn/bin               # cargo libs
 
 localprofile=$DOTFILES/environment/local_profile.sh
 if [[ -f $localprofile ]]; then
--- environment/common_rc.d/aliases.sh
+++ environment/common_rc.d/aliases.sh
@@ -5,10 +5,11 @@ alias ls='ls --color=auto'
 alias ll='ls -Gl'
 alias la='ls -lAhG'
 alias l='ls -CFG'
-# ensure lazy loded nvm and python are present as well as the project specific binaries (eslint, phpunit ...)
-alias nvim='node --version && python --version && PATH=./node_modules/.bin:$PATH nvim'
-alias nv='nvim -u ~/.dotfiles/config/nvim/init.vim'
-# alias emacs='node --version && python --version && emacs'
+# ensure project specific binaries (eslint, phpunit ...)
+alias nvim='PATH=./node_modules/.bin:$PATH nvim'
+alias nv=nvim
+alias vim=nvim
+alias vi=nvim
 
 # tools
 alias free='top -l 1 | grep Phys'
