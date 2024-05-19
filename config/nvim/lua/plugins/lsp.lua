f403dbf30432eaa0b691f20dca7cbac3dc6cc212
Jirgn <jirgn76@googlemail.com>
2024-05-20 00:52:44 +0200

    nvim: lazy enable more lsps

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -3,6 +3,50 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        -- neosfusionls = {},
+        phpactor = {},
+        html = {
+          filetypes = { "html", "xhtml", "heex" },
+        },
+        cssls = {},
+        jsonls = {
+          filetypes = { "json", "jsonc" },
+          settings = {
+            json = {
+              -- Schemas https://www.schemastore.org,
+              schemas = {
+                {
+                  fileMatch = { "package.json" },
+                  url = "https://json.schemastore.org/package.json",
+                },
+                {
+                  fileMatch = { "tsconfig*.json" },
+                  url = "https://json.schemastore.org/tsconfig.json",
+                },
+                {
+                  fileMatch = {
+                    ".prettierrc",
+                    ".prettierrc.json",
+                    "prettier.config.json",
+                  },
+                  url = "https://json.schemastore.org/prettierrc.json",
+                },
+                {
+                  fileMatch = { ".eslintrc", ".eslintrc.json" },
+                  url = "https://json.schemastore.org/eslintrc.json",
+                },
+                {
+                  fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
+                  url = "https://json.schemastore.org/babelrc.json",
+                },
+                {
+                  fileMatch = { "lerna.json" },
+                  url = "https://json.schemastore.org/lerna.json",
+                },
+              },
+            },
+          },
+        },
         elixirls = {
           settings = {
             elixirLS = {
@@ -53,74 +97,3 @@ return {
     },
   },
 }
--- ruff_lsp = {},
---     prolog_ls = {},
---     neosfusionls = {},
---     phpactor = {},
---     rust_analyzer = {},
---     clangd = {},
---     lua_ls = {
---         settings = {
---             Lua = {
---                 runtime = {
---                     version = 'LuaJIT',
---                 },
---                 diagnostics = {
---                     globals = {'vim'},
---                 },
---                 workspace = {
---                     library = vim.api.nvim_get_runtime_file("", true),
---                 },
---                 telemetry = {
---                     enable = false,
---                 },
---             },
---         },
---     },
---     html = {
---         cmd = { "vscode-html-language-server", "--stdio" },
---         filetypes = { "html", "xhtml", "heex" }
---     },
---     cssls = {
---         cmd = { "vscode-css-language-server", "--stdio" },
---     },
---     jsonls = {
---         cmd = {"vscode-json-language-server", "--stdio"},
---         filetypes = {"json", "jsonc"},
---         settings = {
---             json = {
---             -- Schemas https://www.schemastore.org,
---                 schemas = {
---                     {
---                         fileMatch = {"package.json"},
---                         url = "https://json.schemastore.org/package.json"
---                     },
---                     {
---                         fileMatch = {"tsconfig*.json"},
---                         url = "https://json.schemastore.org/tsconfig.json"
---                     },
---                     {
---                         fileMatch = {
---                             ".prettierrc",
---                             ".prettierrc.json",
---                             "prettier.config.json"
---                         },
---                         url = "https://json.schemastore.org/prettierrc.json"
---                     },
---                     {
---                         fileMatch = {".eslintrc", ".eslintrc.json"},
---                         url = "https://json.schemastore.org/eslintrc.json"
---                     },
---                     {
---                         fileMatch = {".babelrc", ".babelrc.json", "babel.config.json"},
---                         url = "https://json.schemastore.org/babelrc.json"
---                     },
---                     {
---                         fileMatch = {"lerna.json"},
---                         url = "https://json.schemastore.org/lerna.json"
---                     }
---                 }
---             }
---         }
---     }
---
