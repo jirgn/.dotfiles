88ae1538a3cc3eefb40168f43855bad8a3c765f4
Jirgn <jirgn76@googlemail.com>
2024-05-20 21:47:29 +0200

    nvim: activate diagnostics float keymap

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,4 @@ local map = vim.keymap.set
 -- convinience
 map("n", "<leader>.", "<C-^>", { desc = "Toggle with last buffer" })
 map("n", "<Esc>", "<cmd>nohlsearch<cr>", { desc = "ensure turnoff hlsearch" })
+map("n", "<leader>xk", "<cmd>lua vim.diagnostic.open_float()<cr>", { desc = "show diagnostic in floating window" })
