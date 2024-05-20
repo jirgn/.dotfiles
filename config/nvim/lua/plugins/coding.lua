8f129ce7093a158640c8bf3c83351dbde526a089
Jirgn <jirgn76@googlemail.com>
2024-05-20 22:27:19 +0200

    nvim: lazy configure codeium ki integration

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -9,4 +9,58 @@ return {
       })
     end,
   },
+  {
+    "Exafunction/codeium.vim",
+    event = "BufEnter",
+    config = function()
+      vim.g.codeium_disable_bindings = 1
+      -- vim.g.codeium_filetypes_disabled_by_default = true
+      -- vim.g.codeium_filetypes = {
+      --   rust = true,
+      --   typescript = true,
+      -- }
+    end,
+    keys = {
+      {
+        "<c-g><cr>",
+        "codeium#Accept()",
+        mode = "i",
+        noremap = true,
+        expr = true,
+        desc = "Accept suggestion",
+      },
+      {
+        "<c-g><c-n>",
+        "codeium#CycleCompletions(1)",
+        mode = "i",
+        noremap = true,
+        expr = true,
+        desc = "Next suggestion",
+      },
+      {
+        "<c-g><c-p>",
+        "codeium#CycleCompletions(-1)",
+        mode = "i",
+        noremap = true,
+        expr = true,
+        desc = "Previous suggestion",
+      },
+      {
+        "<c-g><c-x>",
+        "codeium#Clear()",
+        mode = "i",
+        noremap = true,
+        expr = true,
+        desc = "Clear suggestions",
+      },
+      {
+        "<c-g><c-o>",
+        "codeium#Complete()",
+        mode = "i",
+        noremap = true,
+        expr = true,
+        desc = "Trigger suggestions",
+      },
+    },
+  },
 }
