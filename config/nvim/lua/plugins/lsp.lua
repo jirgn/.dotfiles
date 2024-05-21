e48a213dafe049a1295d1771d63f8b5e3dd89fc3
Jirgn <jirgn76@googlemail.com>
2024-05-21 11:36:00 +0200

    nvim: lazy use pyright via lang.python extra

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -9,44 +9,6 @@ return {
           filetypes = { "html", "xhtml", "heex" },
         },
         cssls = {},
-        jsonls = {
-          filetypes = { "json", "jsonc" },
-          settings = {
-            json = {
-              -- Schemas https://www.schemastore.org,
-              schemas = {
-                {
-                  fileMatch = { "package.json" },
-                  url = "https://json.schemastore.org/package.json",
-                },
-                {
-                  fileMatch = { "tsconfig*.json" },
-                  url = "https://json.schemastore.org/tsconfig.json",
-                },
-                {
-                  fileMatch = {
-                    ".prettierrc",
-                    ".prettierrc.json",
-                    "prettier.config.json",
-                  },
-                  url = "https://json.schemastore.org/prettierrc.json",
-                },
-                {
-                  fileMatch = { ".eslintrc", ".eslintrc.json" },
-                  url = "https://json.schemastore.org/eslintrc.json",
-                },
-                {
-                  fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
-                  url = "https://json.schemastore.org/babelrc.json",
-                },
-                {
-                  fileMatch = { "lerna.json" },
-                  url = "https://json.schemastore.org/lerna.json",
-                },
-              },
-            },
-          },
-        },
         elixirls = {
           settings = {
             elixirLS = {
@@ -75,7 +37,6 @@ return {
         vimls = {},
         bashls = {},
         tsserver = {},
-        pyright = {},
         emmet_language_server = {
           filetypes = {
             "css",
