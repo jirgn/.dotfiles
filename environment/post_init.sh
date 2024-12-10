92947f0ac67ffd5007c236a8d65228a641aa48ff
Jirgn <jirgn76@googlemail.com>
2024-12-10 14:40:38 +0100

    set gnu-grep and gnu-sed as default on macos

--- environment/post_init.sh
+++ environment/post_init.sh
@@ -3,9 +3,12 @@
 # --------------------------
 
 # ensure homebrew is before system paths
-PATH=/opt/homebrew/bin:$PATH              # only MacOS
-PATH=/opt/homebrew/sbin:$PATH             # only MacOS
+PATH=/opt/homebrew/bin:$PATH  # only MacOS
+PATH=/opt/homebrew/sbin:$PATH # only MacOS
+
+## fix gnu-sed and gnu-grep to work with native name -> original is shadowed
+PATH="/opt/homebrew/opt/gnu-sed/libexec/gnubin:$PATH"
+PATH="/opt/homebrew/opt/grep/libexec/gnubin:$PATH"
 
 # tools especially language version switcher need do be on top of the path to be present the relevant bin
 source /opt/homebrew/opt/asdf/libexec/asdf.sh
-
